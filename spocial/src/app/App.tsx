36c0e7faa9055f30ed4e5730216df58275b5db0a
liyu1353 <xwingpilot15@gmail.com>
2024-11-16 14:46:38 -0500

    Imported some from previous project, but ERRORS! please take note.

---
+++ spocial/src/app/App.tsx
@@ -0,0 +1,41 @@
+import "./App.css"
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Post from "./components/Post.jsx";
+import RecipeBook from "./components/RecipeBook.jsx";
+function App() {
+    return(
+        <>
+            <Router>
+                <Routes>
+
+                    <Route
+                        exact
+                        path="/"
+                        element={<Home />}
+                    />
+                    {/* This route is for about component with exact path "/about", in component props we passes the imported component*/}
+                    <Route
+                        path="/Post"
+                        element={<Post />}
+                    />
+                    <Route
+                        path="/RecipeBook"
+                        element={<RecipeBook />}
+                    />
+
+                    <Route
+                        path="*"
+                        element={<Navigate to="/" />}
+                    />
+                </Routes>
+            </Router>
+        </>
+    );
+}
+export default App;
