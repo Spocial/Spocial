88bf53bdf0881035bed6d9b4d89113abf935fec6
daniel <xwingpilot15@gmail.com>
2024-11-18 18:04:09 -0500

    auth file started

---
+++ spocial/src/app/auth.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyByMLKSvWOqEYbW1QOrejutofjbLEFbF4E",
+    authDomain: "spocial-d4624.firebaseapp.com",
+    projectId: "spocial-d4624",
+    storageBucket: "spocial-d4624.firebasestorage.app",
+    messagingSenderId: "474951645442",
+    appId: "1:474951645442:web:21d8eed77b8e0ed68bea1b",
+    measurementId: "G-DCES32WN6X"
+};
+
+const app = initializeApp(firebaseConfig);
